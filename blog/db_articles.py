955ec366b76c903e2f5de496be01b51c883778a0
Abdoulaye <layegaye001@gmail.com>
2024-06-19 21:05:55 +0000

    First commit

---
+++ blog/db_articles.py
@@ -0,0 +1,17 @@
+articles = [
+    {
+        "titre": "Premier article",
+        "date_publication": "01/01/2020",
+        "contenu": "Contenu du premier article"
+    },
+    {
+        "titre": "Deuxiéme article",
+        "date_publication": "02/01/2020",
+        "contenu": "Contenu du deuxiéme article"
+    },
+    {
+        "titre": "Trosiéme article",
+        "date_publication": "03/01/2020",
+        "contenu": "Contenu du troisiéme article"
+    }
+]
